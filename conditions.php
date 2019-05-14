773a1490315594631cf4e79cbb916f8e1375a03f
Iqbal Rony <iqbalrony30@gmail.com>
2019-05-14 14:50:24 +0600

    v-1.01

--- conditions.php
+++ conditions.php
@@ -1,13 +1,21 @@
-/*
- * Single condition:-
+<?php
+/*=================================
+		Single condition:-
+====================================*/
+
+/* Example:- 01
+ * This condition is like this:-
+ * if($post_thumbnail_on_off == 'yes'){
+ *    Show Post Thumbnail
+ * }
 */
 $this->add_control(
 	'post_thumbnail_on_off',
 	[
-		'label' => __( 'Post Thumbnail On/Off?', 'your-plugin' ),
+		'label' => __('Post Thumbnail On/Off?', 'your-plugin'),
 		'type' => \Elementor\Controls_Manager::SWITCHER,
-		'label_on' => __( 'Show', 'your-plugin' ),
-		'label_off' => __( 'Hide', 'your-plugin' ),
+		'label_on' => __('Show', 'your-plugin'),
+		'label_off' => __('Hide', 'your-plugin'),
 		'return_value' => 'yes',
 		'default' => 'yes',
 	]
@@ -15,37 +23,135 @@ $this->add_control(
 $this->add_control(
 	'post_thumbnail',
 	[
-		'label' => __( 'Post Thumbnail', 'your-plugin' ),
+		'label' => __('Post Thumbnail', 'your-plugin'),
 		'type' => \Elementor\Controls_Manager::MEDIA,
 		'condition' => [
-            'post_thumbnail_on_off' => 'yes',
-        ]
+			'post_thumbnail_on_off' => 'yes',
+		]
 	]
 );
 
 
-/*
- * Two or more condition for one control:-
+/* Example:- 02
+ * This condition is like this:-
+ * if( !empty($title) ){
+ *    Show Title color
+ * }
 */
 $this->add_control(
-	'content',
+	'title',
 	[
-		'label' => __('Content', 'digimart_toolkit'),
-		'type' => Controls_Manager::TEXTAREA,
-		'default' => __('contact@digimart.com', 'digimart_toolkit'),
+		'label' => __('Title', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::TEXT,
+		'default' => 'Title',
+	]
+);
+$this->add_control(
+	'title_color',
+	[
+		'label' => __('Title color', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .title' => 'color: {{VALUE}}',
+		],
+		'condition' => [
+			'title!' => '',
+		]
+	]
+);
+
+
+/*==================================================
+		Two or more condition for one control:-
+=====================================================*/
+
+/*
+ * relation ['and', 'or']. default relation value is 'and'
+ * Compare Operators [ '==', '!=', '!==', 'in', '!in', '<', '<=', '>', '>=', '===' ]
+ * default compare operator value is '==='
+ * ['in', '!in'] this two operator use to find array key exist in it or not.
+*/
+
+/* Example:- 01
+ * This condition is like this:-
+ * if( !empty($title_1) && !empty($title_2) ){
+ *    Show Title color
+ * }
+*/
+$this->add_control(
+	'title_1',
+	[
+		'label' => __('Title 1', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::TEXT,
+		'default' => 'Title 1',
+	]
+);
+$this->add_control(
+	'title_2',
+	[
+		'label' => __('Title 2', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::TEXT,
+		'default' => 'Title 2',
+	]
+);
+$this->add_control(
+	'title_color',
+	[
+		'label' => __('Title color', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .title' => 'color: {{VALUE}}',
+		],
+		'condition' => [
+			'title_1!' => '',
+			'title_2!' => '',
+		]
+	]
+);
+
+/* Example:- 02
+ * This condition is like this:-
+ * if( !empty($title_1) || !empty($title_2) ){
+ *    Show Title color
+ * }
+*/
+$this->add_control(
+	'title_1',
+	[
+		'label' => __('Title 1', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::TEXT,
+		'default' => 'Title 1',
+	]
+);
+$this->add_control(
+	'title_2',
+	[
+		'label' => __('Title 2', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::TEXT,
+		'default' => 'Title 2',
+	]
+);
+$this->add_control(
+	'title_color',
+	[
+		'label' => __('Title color', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .title' => 'color: {{VALUE}}',
+		],
 		'conditions' => [
-			'relation' => 'and',
+			'relation' => 'or',
 			'terms' => [
 				[
-					'name' => 'title',
-					'operator' => '==',
+					'name' => 'title_1',
+					'operator' => '!=',
 					'value' => [
-						'yes',
+						'',
 					],
 				],
 				[
-					'name' => 'title2',
-					'operator' => '==',
+					'name' => 'title_2',
+					'operator' => '!=',
 					'value' => [
 						'',
 					],
@@ -55,90 +161,165 @@ $this->add_control(
 	]
 );
 
+/* Example:- 03
+ * This condition is like this:-
+ * if( !empty($title_1) || $title_2 === 'read' ){
+ *    Show Title color
+ * }
+*/
 $this->add_control(
-    'readmore_text',
-    [
-	'label' => __('Label Text', 'themepaw-companion'),
-	'type' => Controls_Manager::TEXT,
-	'default' => __('Read More','themepaw-companion'),
-	'conditions'   => [
-	    'terms' => [
-		[
-		    'relation' => 'or',
-		    'terms'    => [
-			[
-			    'name'  => 'left_meta',
-			    'value' => 'readmore',
-			],
-			[
-			    'name'  => 'right_meta',
-			    'value' => 'readmore',
-			]
-		    ],
+	'title_1',
+	[
+		'label' => __('Title 1', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::TEXT,
+		'default' => 'Title 1',
+	]
+);
+$this->add_control(
+	'title_2',
+	[
+		'label' => __('Title 2', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::TEXT,
+		'default' => 'Title 2',
+	]
+);
+$this->add_control(
+	'title_color',
+	[
+		'label' => __('Title color', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .title' => 'color: {{VALUE}}',
 		],
-	    ],
-	],
-    ]
+		'conditions' => [
+			'terms' => [
+				[
+					'relation' => 'or',
+					'terms' => [
+						[
+							'name' => 'title_1',
+							'operator' => '!=',
+							'value' => '',
+						],
+						[
+							'name' => 'title_2',
+							'value' => 'read',
+						]
+					],
+				],
+			],
+		],
+	]
+);
+
+/* Example:- 04
+ * This condition is like this:-
+ * if( (!empty($title_1) || !empty($title_2)) && $icon=='yes ){
+ *    Show Title color
+ * }
+ * this is advance level condition and use it by the cool head.
+*/
+$this->add_control(
+	'title_1',
+	[
+		'label' => __('Title 1', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::TEXT,
+		'default' => 'Title 1',
+	]
+);
+$this->add_control(
+	'title_2',
+	[
+		'label' => __('Title 2', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::TEXT,
+		'default' => 'Title 2',
+	]
+);
+$this->add_control(
+	'icon',
+	[
+		'label' => __('Icon On/Off?', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::SWITCHER,
+		'label_on' => __('Show', 'your-plugin'),
+		'label_off' => __('Hide', 'your-plugin'),
+		'return_value' => 'yes',
+		'default' => 'yes',
+	]
+);
+$this->add_control(
+	'title_color',
+	[
+		'label' => __('Title color', 'your-plugin'),
+		'type' => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .title' => 'color: {{VALUE}}',
+		],
+		'conditions' => [
+			'terms' => [
+				[
+					'relation' => 'or',
+					'terms' => [
+						[
+							'name' => 'title_1',
+							'operator' => '!=',
+							'value' => '',
+						],
+						[
+							'name' => 'title_2',
+							'operator' => '!=',
+							'value' => '',
+						],
+					],
+				],
+				[
+					'terms' => [
+						[
+							'name' => 'icon',
+							'value' => 'yes',
+						],
+					],
+				]
+			]
+		],
+	]
 );
 
 
 /*
  * condition for URL control:-
 */
+/*===================================================================
+		Condition for those control which return array value:-
+=======================================================================*/
+/* Example:- 01
+ * URL control return an array which contain three key which are url, is_external and nofollow
+ * This condition is like this:-
+ * if( ( !empty($link['url']) ){
+ *    Show Button color
+ * }
+*/
 $this->add_control(
-	'content',
+	'link',
 	[
-		'label' => __('Content', 'digimart_toolkit'),
-		'type' => Controls_Manager::TEXTAREA,
-		'default' => __('contact@digimart.com', 'digimart_toolkit'),
+		'label' => __('Button Link', 'text-domain'),
+		'type' => \Elementor\Controls_Manager::URL,
+		'default' => [
+			'url' => '',
+			'is_external' => true,
+			'nofollow' => true,
+		],
+	]
+);
+$this->add_control(
+	'btn_color',
+	[
+		'label' => __('Button Color', 'text-domain'),
+		'type' => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .button' => 'color: {{VALUE}}',
+		],
 		'condition' => [
-			'title2[url]!' => '',
+			'link[url]!' => '',
 		]
 	]
 );
-
-
-
-
-/*
- * Add a custom control to an existing Elementor widget:-
-*/
-add_action('elementor/element/before_section_end', 'add_control_in_existing_widget', 10, 3 );
-function add_control_in_existing_widget( $section, $section_id, $args ) {
-	if( $section->get_name() == 'testimonial' && $section_id == 'section_testimonial' ){
-		// we are at the end of the "section_image" area of the "image-box"
-		$section->add_control(
-			'testimonial_name_title_pos' ,
-			[
-				'label'        => 'Name and title position',
-				'type'         => Elementor\Controls_Manager::SELECT,
-				'default'      => 'vertical',
-				'options'      => array(
-					'vertical' => 'Vertical',
-					'horizontal' => 'Horizontal'
-				),
-				'prefix_class' => 'dgm-testimonial-name-title-',
-				'label_block'  => true,
-			]
-		);
-	}
-}
-
-add_action( 'elementor/widget/before_render_content', 'custom_render_button' );
-/**
- * Adding a new attribute to our button
- *
- * @param \Elementor\Widget_Base $button
- */
-function custom_render_button( $button ) {
-	//Check if we are on a button
-	if( 'testimonial' === $button->get_name() ) {
-		// Get the settings
-		$settings = $button->get_settings();
-
-		// Adding our type as a class to the button
-		if( $settings['testimonial_name_title_pos'] ) {
-			$button->add_render_attribute( 'testimonial_content', 'class', $settings['testimonial_name_title_pos'], true );
-		}
-	}
-}
